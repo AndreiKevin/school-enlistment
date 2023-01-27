c8b41172715811e386ead8e90b17290c8f662312
Sakui <ethan_saquilayan@dlsu.edu.ph>
2023-01-27 19:30:27 +0800

    add: toString, equals and hashCode to Room class
    :tomato:

--- src/main/java/com/orangeandbronze/enlistment/Room.java
+++ src/main/java/com/orangeandbronze/enlistment/Room.java
@@ -37,4 +37,21 @@ public class Room {
         --currentStudentOccupied;
     }
 
+    @Override
+    public String toString() {
+        return roomName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room room = (Room) o;
+        return Objects.equals(roomName, room.roomName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roomName);
+    }
 }
