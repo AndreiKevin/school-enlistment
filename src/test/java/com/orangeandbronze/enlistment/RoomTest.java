95b62330a787b2eb5ba67f3c3c83ec11d7f4f031
JoaquinDelaRosa <80933658+JoaquinDelaRosa@users.noreply.github.com>
2023-01-31 16:51:02 +0800

    refactor: tests for room should be separate

--- src/test/java/com/orangeandbronze/enlistment/RoomTest.java
+++ src/test/java/com/orangeandbronze/enlistment/RoomTest.java
@@ -9,5 +9,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class RoomTest
 {
+    static final Schedule DEFAULT_SCHEDULE = new Schedule(Days.MTH, Period.H1430);
+    @Test
+    void enlist_room_capacity_full(){
+        //Given 2 students a section that has a room capacity of 1
+        Student student1 = new Student(1);
+        Student student2 = new Student(2);
+        Room room = new Room("AGH20", 1);
+        Section sec = new Section("A", DEFAULT_SCHEDULE, room);
 
+        //When student 1 enlists
+        student1.enlist(sec);
+
+        //Then an exception should be thrown when student 2 enlists
+        assertThrows(SectionCapacityException.class, () -> student2.enlist(sec));
+    }
+
+    @Test
+    void enlist_room_capacity_not_full(){
+        //Given 2 students a section that has a room capacity of 2
+        Student student1 = new Student(1);
+        Student student2 = new Student(2);
+        Room room = new Room("AGH20", 2);
+        Section sec = new Section("A", DEFAULT_SCHEDULE, room);
+
+        //When student 1 enlists
+        student1.enlist(sec);
+
+        //Then student 2 should also be able to enlist
+        assertDoesNotThrow(() -> student2.enlist(sec));
+    }
 }
