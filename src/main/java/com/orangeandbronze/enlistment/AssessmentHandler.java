0c38460001745cd10e91e35c4ff884b5a5698b13
JoaquinDelaRosa <62501229+TheRealAnonymous008@users.noreply.github.com>
2023-02-11 06:57:43 +0800

    refactor: follow newspaper rule for assessmentHandler

--- src/main/java/com/orangeandbronze/enlistment/AssessmentHandler.java
+++ src/main/java/com/orangeandbronze/enlistment/AssessmentHandler.java
@@ -6,9 +6,6 @@ import java.util.*;
 class AssessmentHandler {
     private static final BigDecimal MISCELLANEOUS_FEES = new BigDecimal("3000");
     private static final BigDecimal VAT_MULTIPLIER = new BigDecimal("1.12");
-    private AssessmentHandler() {
-
-    }
 
     static BigDecimal assess(Collection<Section> sections){
         BigDecimal totalFees = getTotalFees(sections);
@@ -23,5 +20,9 @@ class AssessmentHandler {
             totalFees = totalFees.add(sectionIterator.next().getFees());
         }
         return totalFees;
+    }
+
+    private AssessmentHandler() {
+
     }
 }
