425e326be6c5c5e56f21f3b845dc4fc880383c69
Sakui <63701743+Sakui-ph@users.noreply.github.com>
2023-02-14 15:21:25 +0800

    add: No two sections can share the same room if their schedules overlap test
    :airplane:

---
+++ src/test/java/com/orangeandbronze/enlistment/SectionTest.java
@@ -0,0 +1,19 @@
+package com.orangeandbronze.enlistment;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class SectionTest {
+    private static final Schedule DEFAULT_SCHEDULE = new Schedule(Days.MTH, Period.H1430);
+    private static final Subject DEFAULT_SUBJECT_A = new Subject("A", 1);
+    @Test
+    void sections_cannot_have_same_room_and_schedule() {
+        // Given a section with a room and schedule
+        Room room = new Room("AGH20", 45);
+        Section section = new Section("1", DEFAULT_SCHEDULE, room, DEFAULT_SUBJECT_A);
+
+        // When a new section with the same room and schedule is made
+        // Then throw an error
+        assertThrows(SectionOverlapException.class, () -> new Section("2", DEFAULT_SCHEDULE, room, DEFAULT_SUBJECT_A));
+    }
+}
