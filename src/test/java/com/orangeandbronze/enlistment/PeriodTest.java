51d316245ad2064999ae3e037bb04a2d51381ab1
JoaquinDelaRosa <62501229+TheRealAnonymous008@users.noreply.github.com>
2023-02-16 19:44:44 +0800

    refactor: remove unused constants

--- src/test/java/com/orangeandbronze/enlistment/PeriodTest.java
+++ src/test/java/com/orangeandbronze/enlistment/PeriodTest.java
@@ -3,10 +3,6 @@ package com.orangeandbronze.enlistment;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 public class PeriodTest {
-    private static final Period DEFAULT_PERIOD = new Period(830, 1100);
-    private static final Schedule DEFAULT_SCHEDULE = new Schedule(Days.MTH, DEFAULT_PERIOD);
-    private static final Subject DEFAULT_SUBJECT_A = new Subject("A", 1);
-    private static final Subject DEFAULT_SUBJECT_B = new Subject("B", 1);
     @Test
     void end_before_start_time_throws_exception() {
         // Given nothing
